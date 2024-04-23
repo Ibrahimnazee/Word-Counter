cfad74bf1f1e4fd62fecbe4f44e47e07fdfe97d2
Muhammad Ibrahim <160228904+Ibrahimnazee@users.noreply.github.com>
2024-04-23 18:34:54 +0500

    updated

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+console.log(chalk.bold.bgCyanBright( "\n \t \t Welcome To Word Counter"));
+console.log(chalk.green("=".repeat(60)));
+let answers = await inquirer.prompt([
+    {
+        name : "sentence",
+        type:  "input",
+        message: "Enter A Word",
+    }
+])
+
+let words = answers.sentence.trim().split(" ");
+console.log(words);
+console.log(chalk.bold.bgBlueBright("\n \t \t -sentence word"));
+console.log(chalk.bold.bgRedBright(`\n \t \t word count ${(chalk.bold.bgRedBright(words.length))}`));
+console.log(chalk.green("=".repeat(60)));
